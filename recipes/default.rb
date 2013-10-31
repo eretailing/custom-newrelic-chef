5ae01a2eff309e71e688311224548f17a0906148
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-31 15:16:00 +0200

    Update default.rb
    remove apt_repository dependency

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,12 +10,18 @@ unless node["new_relic"]["license_key"].empty?
   case node['platform']
   when "debian", "ubuntu"
     
-    apt_repository "newrelic" do
-      uri "http://apt.newrelic.com/debian/"
-      components ["newrelic", "non-free"]
-      key "548C16BF"
-      keyserver node["new_relic"]["keyserver"]
-      action :add
+    execute "add newrelic deb repository" do
+      user 'root'
+      group 'root'
+      command <<-EOF
+        echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' > /etc/apt/sources.list.d/newrelic.list
+        wget -q -O - https://download.newrelic.com/548C16BF.gpg | apt-key add -
+        apt-get update
+      EOF
+      not_if do
+        find = Mixlib::ShellOut.new("find /etc/apt/ -name '*.list' | xargs cat | grep  ^[[:space:]]*deb.*[[:space:]]newrelic[[:space:]] | grep -v deb-src").run_command
+        find.exitstatus && find.stdout.length>1
+      end
     end
 
   when "redhat", "centos", "amazon", "scientific"
