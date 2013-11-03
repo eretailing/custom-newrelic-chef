596e0cec74fa06b28d6831c3a8d1a03a9c3f03f0
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-03 17:06:51 +0200

    use chef-rewind

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,5 @@
-#
-# Cookbook Name:: newrelic-sysmond
-# Recipe:: default
-#
-# Copyright 2011-2012, Phil Cohen
-#
+chef_gem "chef-rewind"
+require 'chef/rewind'
 
 unless node["new_relic"]["license_key"].empty?
 
@@ -24,27 +20,14 @@ unless node["new_relic"]["license_key"].empty?
       end
     end
 
+    include_recipe "chef-newrelic-sysmond::default"
+    
+    unwind "apt_repository[newrelic]"
+    
   when "redhat", "centos", "amazon", "scientific"
 
-    execute "Add New Relic yum repository" do
-      command "rpm -Uvh http://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm"
-      not_if "yum list installed | grep newrelic-repo.noarch"
-    end
-
-  end
-
-  package "newrelic-sysmond"
-
-  template "/etc/newrelic/nrsysmond.cfg" do
-    source "nrsysmond.cfg.erb"
-    owner "root"
-    group "newrelic"
-    mode 0640
-    notifies :restart, "service[newrelic-sysmond]"
-  end
-
-  service "newrelic-sysmond" do
-    action [:enable, :start]
+    include_recipe "chef-newrelic-sysmond::default"
+    
   end
 
 end
