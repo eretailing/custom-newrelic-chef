3ec0ec7477e48cb98ddcd1d130302e3b5203a155
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-06 13:55:32 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,12 @@ name             "shlomo-newrelic"
 maintainer       "Shlomo Swidler"
 maintainer_email "shlomo.swidler@orchestratus.com"
 license          "MIT"
-description      "Setup New Relic sysmond for server monitoring"
+description      "Setup New Relic"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.1.0"
 
 recipe "newrelic-sysmond", "Install and configure newrelic-sysmond"
+recipe "php", "Install and configure newrelic-php5"
 
 supports "debian"
 supports "ubuntu"
