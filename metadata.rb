f0273ead48b7b870b403d0034d23a0d7b2ade672
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-07 16:24:33 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Setup New Relic"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.1.0"
 
-recipe "newrelic-sysmond", "Install and configure newrelic-sysmond"
+recipe "default", "Install and configure newrelic-sysmond"
 recipe "php", "Install and configure newrelic-php5"
 
 supports "debian"
@@ -14,4 +14,4 @@ supports "ubuntu"
 supports "redhat", ">= 5.0"
 supports "centos", ">= 5.0"
 
-depends "chef-newrelic-sysmond", "= 1.3.3"
+depends "newrelic-sysmond", "= 1.3.3"
