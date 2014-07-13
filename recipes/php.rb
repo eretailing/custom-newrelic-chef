619ee17060a037a3d9b1054b1c8fa7cb8640fdba
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-13 19:03:18 +0300

    Add new newrelic license location
    Some newrelic installs keep the license key in /etc/php5/conf.d/newrelic. Set it up there if needed.

--- recipes/php.rb
+++ recipes/php.rb
@@ -13,4 +13,10 @@ unless node[:new_relic][:license_key].nil?
     })
     command "newrelic-install install"
   end
+  
+  # Some editions have the license key in an additional location
+  execute "Update newrelic license key" do
+    command %Q{sed -i -e "s/newrelic.license = \"\"/newrelic.license=\"#{node[:new_relic][:license_key]}\"/" /etc/php5/conf.d/newrelic.ini }
+    only_if { File.exists?("/etc/php5/conf.d/newrelic.ini") }
+  end
 end
