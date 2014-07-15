f3f3b38e6aa5d098184e7e078586f57a493d02f4
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-15 12:04:41 +0300

    Update php.rb

--- recipes/php.rb
+++ recipes/php.rb
@@ -6,7 +6,7 @@ unless node[:new_relic][:license_key].nil?
 
   package 'newrelic-php5'
 
-  execute "initialize newrelic" do
+  execute "initialize newrelic PHP" do
     environment ({
       "NR_INSTALL_SILENT" => "true",
       "NR_INSTALL_KEY" => node[:new_relic][:license_key]
@@ -15,7 +15,7 @@ unless node[:new_relic][:license_key].nil?
   end
   
   # Some editions have the license key in an additional location
-  execute "Update newrelic license key" do
+  execute "Update newrelic PHP license key" do
     command %Q{sed -i -e "s/newrelic.license = \"\"/newrelic.license=\"#{node[:new_relic][:license_key]}\"/" /etc/php5/conf.d/newrelic.ini }
     only_if { File.exists?("/etc/php5/conf.d/newrelic.ini") }
   end
