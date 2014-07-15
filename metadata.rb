da90c3ece16893693b1affd441ce37c7f14705c5
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-15 11:35:46 +0300

    Bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "shlomo.swidler@orchestratus.com"
 license          "MIT"
 description      "Setup New Relic"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
+version          "0.2.0"
 
 recipe "default", "Install and configure newrelic-sysmond"
 recipe "php", "Install and configure newrelic-php5"
