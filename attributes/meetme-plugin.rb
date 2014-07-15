d9e93fa245ec7bdb027a9f7b7a1b72745a19bc1d
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-15 13:39:48 +0300

    Fix typos

--- attributes/meetme-plugin.rb
+++ attributes/meetme-plugin.rb
@@ -1,15 +1,15 @@
 # Which plugins will be activated?
-# The configuration for each activated plugin must be in a like-named node under [:new_relic][:meetme-plugin][:plugins]
-default[:new_relic][:meetme-plugin][:activate_plugins] = [ 'apache_httpd', 'php_apc' ]
+# The configuration for each activated plugin must be in a like-named node under [:new_relic][:meetme_plugin][:plugins]
+default[:new_relic][:meetme_plugin][:activate_plugins] = [ 'apache_httpd', 'php_apc' ]
 
-default[:new_relic][:meetme-plugin][:plugins][:apache_httpd][:scheme] = 'http'
-default[:new_relic][:meetme-plugin][:plugins][:apache_httpd][:host] = 'localhost'
-default[:new_relic][:meetme-plugin][:plugins][:apache_httpd][:verify_ssl_cert] = 'false'
-default[:new_relic][:meetme-plugin][:plugins][:apache_httpd][:port] = '80'
-default[:new_relic][:meetme-plugin][:plugins][:apache_httpd][:path] = '/server-status'
+default[:new_relic][:meetme_plugin][:plugins][:apache_httpd][:scheme] = 'http'
+default[:new_relic][:meetme_plugin][:plugins][:apache_httpd][:host] = 'localhost'
+default[:new_relic][:meetme_plugin][:plugins][:apache_httpd][:verify_ssl_cert] = 'false'
+default[:new_relic][:meetme_plugin][:plugins][:apache_httpd][:port] = '80'
+default[:new_relic][:meetme_plugin][:plugins][:apache_httpd][:path] = '/server-status'
 
-default[:new_relic][:meetme-plugin][:plugins][:php_apc][:scheme] = 'http'
-default[:new_relic][:meetme-plugin][:plugins][:php_apc][:host] = 'localhost'
-default[:new_relic][:meetme-plugin][:plugins][:php_apc][:verify_ssl_cert] = 'false'
-default[:new_relic][:meetme-plugin][:plugins][:php_apc][:port] = '80'
-default[:new_relic][:meetme-plugin][:plugins][:php_apc][:path] = '/apc-nrp.php'
+default[:new_relic][:meetme_plugin][:plugins][:php_apc][:scheme] = 'http'
+default[:new_relic][:meetme_plugin][:plugins][:php_apc][:host] = 'localhost'
+default[:new_relic][:meetme_plugin][:plugins][:php_apc][:verify_ssl_cert] = 'false'
+default[:new_relic][:meetme_plugin][:plugins][:php_apc][:port] = '80'
+default[:new_relic][:meetme_plugin][:plugins][:php_apc][:path] = '/apc-nrp.php'
