678cff47c27f2a64bed2fca4e87b76a88682c0cc
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-15 23:50:05 +0300

    add newrelic to sudoers

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -1,5 +1,11 @@
 package 'ruby1.9.3' # for the upcoming newrelic plugin. It's written in Ruby.
 # the upcoming newrelic recipe relies on the 'newrelic' user being able to install system gems via sudo.
 # make that happen without asking for a password
-
+file "/etc/sudoers.d/newrelic-bundle-install" do
+  owner "root"
+  group "root"
+  mode 0440
+  action :create
+  content "newrelic ALL=(root)NOPASSWD:bundle install"
+end
 include_recipe 'newrelic_plugins::aws_cloudwatch'
