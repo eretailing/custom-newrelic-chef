93ba2da08fc051020c69310d8b1b1a2ed7304829
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-15 11:36:44 +0300

    Add description of meetme-plugin recipe

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,7 @@ version          "0.2.0"
 
 recipe "default", "Install and configure newrelic-sysmond"
 recipe "php", "Install and configure newrelic-php5"
+recipe "meetme-plugin", "Install and configure the MeetMe Plugin Agent"
 
 supports "debian"
 supports "ubuntu"
